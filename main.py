86b2be3c381ee71e7255f50d28f27e7819987a84
sahalashfaq <145576065+sahalashfaq@users.noreply.github.com>
2025-09-12 06:33:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from streamlit_autorefresh import st_autorefresh
 st.set_page_config(page_title="Weather Dashboard", layout="wide")
 
 # Auto-refresh every 10s
-st_autorefresh(interval=2_000, limit=None, key="weather_refresh")
+st_autorefresh(interval=10_000, limit=None, key="weather_refresh")
     
 st.markdown("""<style>
             header{
@@ -62,4 +62,5 @@ except Exception as e:
     st.error(f"Error: {e}")
 
     
+
 
